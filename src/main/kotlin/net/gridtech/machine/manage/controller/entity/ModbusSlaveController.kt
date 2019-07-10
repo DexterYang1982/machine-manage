a42d86798e387d6e3ab5110eeea9a8d52b15753f
Dexter Yang <dexter.yang@qq.com>
2019-07-10 17:52:43 +0800

    init

---
+++ src/main/kotlin/net/gridtech/machine/manage/controller/entity/ModbusSlaveController.kt
@@ -0,0 +1,21 @@
+package net.gridtech.machine.manage.controller.entity
+
+import net.gridtech.core.data.INode
+import net.gridtech.core.util.generateId
+import net.gridtech.machine.model.entity.ModbusSlave
+import net.gridtech.machine.model.entityClass.ModbusSlaveClass
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/api/modbusSlave"], produces = [MediaType.APPLICATION_JSON_VALUE])
+class ModbusSlaveController : EntityController() {
+    override fun addEntity(entityClassId: String, parentId: String, name: String, alias: String): INode? =
+            bootService.dataHolder.entityClassHolder[entityClassId]?.let { entityClass ->
+                if (entityClass is ModbusSlaveClass)
+                    ModbusSlave(generateId(), entityClass).addNew(parentId, name, alias)
+                else
+                    null
+            }
+}
