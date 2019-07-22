bda602846bffaee9badaea86707a21119751f21f
Dexter Yang <dexter.yang@qq.com>
2019-07-22 17:55:22 +0800

    init

--- src/main/kotlin/net/gridtech/machine/manage/controller/entity/TunnelController.kt
+++ src/main/kotlin/net/gridtech/machine/manage/controller/entity/TunnelController.kt
@@ -1,12 +1,16 @@
 package net.gridtech.machine.manage.controller.entity
 
 import net.gridtech.core.data.INode
+import net.gridtech.core.util.cast
 import net.gridtech.core.util.generateId
+import net.gridtech.machine.manage.controller.result
 import net.gridtech.machine.model.entity.Tunnel
 import net.gridtech.machine.model.entityClass.TunnelClass
+import net.gridtech.machine.model.property.entity.TunnelTransaction
+import net.gridtech.machine.model.property.entity.TunnelTransactionPhase
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(value = ["/api/Tunnel"], produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -18,4 +22,64 @@ class TunnelController : EntityController() {
                 else
                     null
             }
+
+    @RequestMapping(value = ["/updateMainCabin"], method = [RequestMethod.PUT])
+    fun updateMainCabin(@RequestParam("id")
+                        id: String,
+                        @RequestParam("mainCabinId")
+                        mainCabinId: String): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.updateMainCabin(mainCabinId))
+
+    @RequestMapping(value = ["/addTransaction"], method = [RequestMethod.POST])
+    fun addTransaction(@RequestParam("id")
+                       id: String,
+                       @RequestBody
+                       transaction: TunnelTransaction): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.addTransaction(transaction))
+
+
+    @RequestMapping(value = ["/updateTransaction"], method = [RequestMethod.PUT])
+    fun updateTransaction(@RequestParam("id")
+                          id: String,
+                          @RequestBody
+                          transaction: TunnelTransaction): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.updateTransaction(transaction))
+
+
+    @RequestMapping(value = ["/deleteTransaction"], method = [RequestMethod.DELETE])
+    fun deleteTransaction(@RequestParam("id")
+                          id: String,
+                          @RequestParam("transactionId")
+                          transactionId: String): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.deleteTransaction(transactionId))
+
+
+    @RequestMapping(value = ["/addTransactionPhase"], method = [RequestMethod.POST])
+    fun addTransactionPhase(@RequestParam("id")
+                            id: String,
+                            @RequestParam("transactionId")
+                            transactionId: String,
+                            @RequestBody
+                            phase: TunnelTransactionPhase): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.addTransactionPhase(transactionId, phase))
+
+
+    @RequestMapping(value = ["/updateTransactionPhase"], method = [RequestMethod.PUT])
+    fun updateTransactionPhase(@RequestParam("id")
+                               id: String,
+                               @RequestParam("transactionId")
+                               transactionId: String,
+                               @RequestBody
+                               phase: TunnelTransactionPhase): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.updateTransactionPhase(transactionId, phase))
+
+
+    @RequestMapping(value = ["/deleteTransactionPhase"], method = [RequestMethod.DELETE])
+    fun deleteTransactionPhase(@RequestParam("id")
+                               id: String,
+                               @RequestParam("transactionId")
+                               transactionId: String,
+                               @RequestParam("phaseId")
+                               phaseId: String): ResponseEntity<*> =
+            result(bootService.dataHolder.entityHolder[id]?.let { cast<Tunnel>(it) }?.description?.deleteTransactionPhase(transactionId, phaseId))
 }
